18fe4cdad0ff5dffb7be8ba11dc6f6fabc0118ed
takeokunn <bararararatty@gmail.com>
2019-06-12 19:05:01 +0900

    Update

--- main.js
+++ main.js
@@ -3,29 +3,46 @@ require('dotenv').config();
 const SlackWebhook = require('slack-webhook');
 const { Auth, Connection, Record, App } = require('@kintone/kintone-js-sdk');
 
+const kintoneTemplate = `
+# 本日の業務
+
+*
+*
+*
+
+
+# 明日の予定
+
+*
+*
+*
+
+# 一言
+`;
+
+
 const handleSlackPostMessage = (text) => {
     const url = process.env.SLACK_WEBHOOK_URL;
     const channel = process.env.SLACK_CHANNEL;
     const slack = new SlackWebhook(url);
     slack.send({
-        text: 'some text',
-        username: 'new username',
-        icon_emoji: ':scream_cat:',
-        channel: '#another-channel'
+        text: text,
+        username: 'take bot',
+        icon_emoji: ':lisp_alien:'
     });
 };
 
 const handleGenerateRecord = () => {
     const record = {
         "content": {
-            "value": "おれのかんぺきなにっぽう:stub_parrot:"
+            "value": kintoneTemplate
         }
     };
     return record;
 };
 
-const fetchKintoneAddRecord = (app_id, record, success, failure) => {
-    record.addRecord(app_id, record)
+const fetchKintoneAddRecord = (record, app_id, my_record, success, failure) => {
+    record.addRecord(app_id, my_record)
         .then(res => success(res))
         .catch((err) => failure(err));
 };
@@ -48,16 +65,17 @@ const handleKintoneAddRecord = (success, failure) => {
     const app = new App(conn);
 
     const my_record = handleGenerateRecord();
-    fetchKintoneAddRecord(app_id, my_record, success, failure);
+    fetchKintoneAddRecord(record, app_id, my_record, success, failure);
 };
-
 
 const main = () => {
     const success = (res) => {
-        console.log(res);
+        const app_id = process.env.KINTONE_APP_ID;
+        const kintone_url = `日報: https://uuum.cybozu.com/k/${app_id}/show#record=${res.id}`;
+        handleSlackPostMessage(kintone_url);
     };
-    const failure = (err) => {
-        console.log(err);
-    };
+    const failure = err => console.log(err);
     handleKintoneAddRecord(success, failure);
 };
+
+main();
